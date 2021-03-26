bf7058faeb96d26646186cc456b064103bbb29a5
dartmax <promostend@gmail.com>
2021-03-26 14:50:18 +0200

    Updates

---
+++ precache-manifest.ec28215fe2229eea1a772ca2208d1993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfaafe13a70bfe8c77a93ccebc6204a",
+    "url": "/TwitterMaterialUI/index.html"
+  },
+  {
+    "revision": "e68e70e662fa8df485b5",
+    "url": "/TwitterMaterialUI/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "227c3e95245f2caad1c0",
+    "url": "/TwitterMaterialUI/static/js/2.2ff6778a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/TwitterMaterialUI/static/js/2.2ff6778a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68e70e662fa8df485b5",
+    "url": "/TwitterMaterialUI/static/js/main.1d473f7f.chunk.js"
+  },
+  {
+    "revision": "2686c5523440e8096b40",
+    "url": "/TwitterMaterialUI/static/js/runtime-main.95177179.js"
+  }
+]);
